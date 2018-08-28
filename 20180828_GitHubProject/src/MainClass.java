ba65ae22d4c9e9719e586976a50e7a4c47907d55
sist <sist@sist48>
2018-08-28 15:50:16 +0900

    nhbvk

--- 20180828_GitHubProject/src/MainClass.java
+++ 20180828_GitHubProject/src/MainClass.java
@@ -3,9 +3,9 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("asasasas");
+		System.out.println("asas");
 		System.out.println("asasasas");
-		System.out.println("asasasas");
-		System.out.println("aaa");
+		System.out.println("aaaaa");
 	
 	}
 
---
+++ 20180828_GitHubProject/src/sfadf.java
@@ -0,0 +1,9 @@
+
+public class sfadf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("oioih");
+	}
+
+}
