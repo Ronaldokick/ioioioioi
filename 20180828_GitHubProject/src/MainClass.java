0310b01a6b58a879d2d2aac1b469033ea1e60ed1
sist <sist@sist48>
2018-08-28 15:37:01 +0900

    vgjhcgjcfgj

--- 20180828_GitHubProject/src/MainClass.java
+++ 20180828_GitHubProject/src/MainClass.java
@@ -4,6 +4,14 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("asasasas");
 		System.out.println("asasasas");
+		System.out.println("asasasas");
+		System.out.println("asasasas");
+		System.out.println("asasasas");
+		System.out.println("asasasas");
+		System.out.println("asasasas");
+		System.out.println("asasasas");
+		System.out.println("asasasas");
+		System.out.println("asasasas");
 	}
 
 }
